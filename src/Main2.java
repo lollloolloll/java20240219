370b22739832c023a3c843600a14dfd9c5fde281
ganghyeon jo <ganghyeon4479@naver.com>
2024-02-19 12:41:22 +0900

    처음 만들었어요 자바 코드

---
+++ src/Main2.java
@@ -0,0 +1,9 @@
+public class Main2 {
+    public static void main(String[] args) {
+        System.out.println("새파일에서 인사");
+        System.out.println("안녕 새파일");
+
+    }
+
+
+}
---
+++ src/Main3.java
@@ -0,0 +1,14 @@
+public class Main3 {
+    public static void main(String[] args) {
+        System.out.println("hi");
+        System.out.println("this is java");
+        System.out.println("this is java");
+        System.out.println("this is java");
+        System.out.println("this is java");
+
+
+
+
+
+    }
+}
